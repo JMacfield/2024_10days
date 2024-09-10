098c67bc22eb07c5d6799e457c076b39c4ca5cc9
k022g0093@g.neec.ac.jp <k022g0093@g.neec.ac.jp>
2024-09-10 17:08:19 +0900

    hCreat

--- Project/Game/GameObject/Enemy/Beam/Beam.h
+++ Project/Game/GameObject/Enemy/Beam/Beam.h
@@ -1,5 +1,51 @@
 #pragma once
+#include "Model.h"
+#include "AnimationModel.h"
+#include "TextureManager.h"
+#include "Camera.h"
+#include "GameObject/Player.h"
+#include "VectorCalculation.h"
+
+#include "Collision.h"
+#include "OBB.h"
+
 class Beam
 {
+public:
+	//レーザーの寿命
+	static const uint32_t kLifeTime = 60 * 10;
+
+	void Initialize();
+
+	void Update();
+
+	void Draw(const Camera& camera);
+
+	/*void OnCollision(Collider* collider) override;
+
+	const Vector3 GetWorldPosition() const override;
+
+	const WorldTransform& GetWorldTransform() const override { return worldTransform_; };*/
+
+	const bool GetIsDead() const { return isDead_; };
+
+private:
+	//モデル
+	std::unique_ptr<Model> model_ = nullptr;
+
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_{};
+
+	//死亡フラグ
+	bool isDead_ = false;
+
+	//タイマー
+	uint32_t lifeTimer_ = 0;
+
+	//OBB
+	OBB obbSize{};
+
+	//目標スケール
+	Vector3 targetScale_{ 1.5f,1.5f,50.0f };
 };
 
