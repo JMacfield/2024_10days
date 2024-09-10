4f60befcf24f8ba6505c1d10b16503690ce82628
canon1002 <k022g0038@g.neec.ac.jp>
2024-09-10 14:40:53 +0900

    タイトル途中

--- Project/Game/GameScene/TitleScene.cpp
+++ Project/Game/GameScene/TitleScene.cpp
@@ -15,7 +15,24 @@ TitleScene::TitleScene()
 void TitleScene::Initialize()
 {
 
+	// -- タイトルのUI -- //
+	titleUITex_[0] = TextureManager::LoadTexture("Resources/AssignmentTexture/UI/title.png");
+	titleUITex_[1] = TextureManager::LoadTexture("Resources/AssignmentTexture/UI/start.png");
+	titleUITex_[2] = TextureManager::LoadTexture("Resources/AssignmentTexture/UI/0.png");
+	titleUITex_[3] = TextureManager::LoadTexture("Resources/AssignmentTexture/UI/exit.png");
+	
+	for (int32_t i = 0; i < titleUI_.size(); i++) {
+		titleUI_[i].reset(Sprite::Create(titleUITex_[i], Vector2(640.0f, float(i + 1) * 80.0f + 260.0f)));
+		titleUI_[i]->SetAnchorPoint(Vector2(0.5f, 0.5f));
+	}
 
+	titleUI_[0]->SetScale(Vector2(1.5f, 1.5f));
+	titleUI_[0]->SetPosition(Vector2(640.0f, 140.0f));
+
+
+	selectNumber_ = 0;;
+	normalT_ = 0.0f;
+	isUpper_ = true;
 
 }
 
@@ -31,6 +48,7 @@ void TitleScene::Update(GameManager* gameManager)
 	}
 	
 
+
 #ifdef _DEBUG
 
 	ImGui::Begin("Debug");
@@ -44,7 +62,10 @@ void TitleScene::Update(GameManager* gameManager)
 
 void TitleScene::Draw()
 {
-
+	for (int32_t i = 0; i < titleUI_.size(); i++) {
+		if (i == 2) { continue; }
+		titleUI_[i]->Draw();
+	}
 }
 
 TitleScene::~TitleScene()
